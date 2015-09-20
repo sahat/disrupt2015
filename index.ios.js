fddf23f2dcf994fc201dd280cbed4ddd3132d2ab
Sahat Yalkabov <sakhat@gmail.com>
2015-09-20 03:37:38 -0700

    updated mock data

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,7 @@
 var React = require('react-native');
 var NavigationBar = require('react-native-navbar');
 var { Icon } = require('react-native-icons');
-
+var { find } = require('lodash');
 var {
   Image,
   ListView,
@@ -28,11 +28,117 @@ var {
   } = React;
 
 
-var disrupt2015 = React.createClass({
-  statics: {
-    title: '<NavigatorIOS>',
-    description: 'iOS navigation capabilities',
+let mockData = [
+  {
+    status: 'ready',
+    customer: {
+      name: 'Dan Seider',
+      photo: 'http://i.imgur.com/XWRhrUC.jpg'
+    },
+    pickupLocation: 'A',
+    items: [
+      {
+        title: 'Men\'s Solid Dress Shirt with Matching Tie',
+        category: 'Men\s Woven Shirts',
+        price: 12,
+        size: 'S',
+        color: 'Charcoal',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-8ad7/k2-_1e1f4d10-8ce1-4b1c-8366-7bd10c493afb.v1.jpg',
+        done: true
+      },
+      {
+        title: 'Fruit of the Loom Men\'s Short Sleeve Pocket Tee',
+        category: 'Men\'s T-Shirts & Tank Tops',
+        price: 3.45,
+        size: 'S',
+        color: 'Heather Blue',
+        quantity: 2,
+        image: 'http://i5.walmartimages.com/dfw/dce07b8c-797c/k2-_190fe3c6-a0c1-4217-b17b-35aee5f21fbd.v2.jpg',
+        done: true
+
+      },
+      {
+        title: 'Fruit of the Loom men\'s 12 pack of crew socks',
+        category: 'Men\'s Socks',
+        price: 8.77,
+        size: 'ONE',
+        color: 'Black',
+        quantity: 1,
+        image: 'http://i5.walmartimages.com/dfw/dce07b8c-fc12/k2-_7a9f4961-082a-41a2-8625-f86fbae6c988.v3.jpg',
+        done: true
+      }
+    ]
   },
+  {
+    status: 'in_progress',
+    customer: {
+      name: 'Elizabeth W. Davis',
+      photo: 'http://i.imgur.com/HJQ6b3M.jpg'
+    },
+    pickupTime: '5:30 PM',
+    items: [
+      {
+        title: 'Suddenly Slim by Catalina Women\'s Slimming Origami',
+        category: 'Women\'s Swimwear',
+        price: 17.30,
+        size: 'L',
+        color: 'Rich Black',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-3265/k2-_88891af7-43e2-4101-8b01-b9fb0a2e3de0.v1.jpg',
+        done: true
+      },
+      {
+        title: 'Fourteenth Place Women\'s Tiered Applique Scoop Neck Top',
+        category: 'Women\'s Tank Tops',
+        price: 8.95,
+        size: 'S',
+        color: 'Black',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-ff07/k2-_2a609791-dce8-4b9c-8e18-2ea4b11ba22c.v3.jpg'
+      },
+      {
+        title: 'Faded Glory Women\'s Super Soft French Terry Skinny Jeans',
+        category: 'Women\'s Jeans',
+        price: 11.00,
+        size: 4,
+        color: 'Medium Wash',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-cf8a/k2-_0699b85d-f33f-4d33-8ff6-e6bb3741eb8e.v2.jpg'
+      },
+      {
+        title: 'Danskin Now Women\'s Dri-More Full Zip Core Hoodie',
+        category: 'Women\'s Activewear',
+        price: 11.88,
+        size: 'S',
+        color: 'Heather Grey',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-eca7/k2-_e1b71dac-9f0c-432f-8e58-51ec8e0f8df8.v1.jpg'
+      }
+    ]
+  },
+  {
+    status: 'received',
+    customer: {
+      name: 'Eugene V. Lane',
+      photo: 'http://i.imgur.com/yQaVS1f.jpg'
+    },
+    pickupTime: '8:00 PM',
+    items: [
+      {
+        title: 'Xbox One 1 TB FIFA 16 Limited Edition Console Bundle',
+        category: 'Video Game Consoles',
+        price: 399.99,
+        size: 'N/A',
+        color: 'N/A',
+        quantity: 1,
+        image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-4322/k2-_76165eac-7764-4266-af5c-ff2efe92fc4e.v1.jpg'
+      }
+    ]
+  }
+];
+
+var disrupt2015 = React.createClass({
 
   render: function() {
     return (
@@ -56,103 +162,12 @@ class EmptyPage extends React.Component {
     super(props);
     var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
-      dataSource: ds.cloneWithRows([
-        {
-          status: 'ready',
-          customer: {
-            name: 'Dan Seider',
-            photo: 'http://i.imgur.com/XWRhrUC.jpg'
-          },
-          pickupLocation: 'A',
-          items: [
-            {
-              title: 'Men\'s Solid Dress Shirt with Matching Tie',
-              walmartNumber: '554222918',
-              price: 12,
-              size: 'S',
-              color: 'Charcoal',
-              quantity: 1,
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-8ad7/k2-_1e1f4d10-8ce1-4b1c-8366-7bd10c493afb.v1.jpg-49fabc758118a5f19d75ca5096137a2550d560e1-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-ff07/k2-_2a609791-dce8-4b9c-8e18-2ea4b11ba22c.v3.jpg-b9a07f3ea413c5a38f76a34ba7365e1404207a83-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-20a3/k2-_64c47dad-5d13-4f20-82fb-caa3a793d9ab.v1.jpg-5873a66dd7e0ed3ac2db50252fa0ec12f9c30999-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-5433/k2-_30a6bd22-4a19-4be8-bdb3-490d3ae31a4c.v6.jpg-9e658601edc1d17dc97bc281105c01afb13c9855-webp-450x450.jpg'
-            }
-          ]
-        },
-        {
-          status: 'in_progress',
-          customer: {
-            name: 'Casey C. Roberts',
-            photo: 'http://i.imgur.com/rTwvkDN.jpg'
-          },
-          pickupTime: '6:15 PM',
-          items: [
-            {
-              image: 'http://i5.walmartimages.com/dfw/dce07b8c-1b24/k2-_6883b400-953a-4171-9da3-3cac0b502f47.v1.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-ff07/k2-_2a609791-dce8-4b9c-8e18-2ea4b11ba22c.v3.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-20a3/k2-_64c47dad-5d13-4f20-82fb-caa3a793d9ab.v1.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-5433/k2-_30a6bd22-4a19-4be8-bdb3-490d3ae31a4c.v6.jpg'
-            }
-          ]
-        },
-        {
-          status: 'in_progress',
-          customer: {
-            name: 'Elizabeth W. Davis',
-            photo: 'http://i.imgur.com/HJQ6b3M.jpg'
-          },
-          pickupTime: '5:30 PM',
-          items: [
-            {
-              image: 'http://i5.walmartimages.com/dfw/dce07b8c-1b24/k2-_6883b400-953a-4171-9da3-3cac0b502f47.v1.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-ff07/k2-_2a609791-dce8-4b9c-8e18-2ea4b11ba22c.v3.jpg-b9a07f3ea413c5a38f76a34ba7365e1404207a83-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-20a3/k2-_64c47dad-5d13-4f20-82fb-caa3a793d9ab.v1.jpg-5873a66dd7e0ed3ac2db50252fa0ec12f9c30999-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-5433/k2-_30a6bd22-4a19-4be8-bdb3-490d3ae31a4c.v6.jpg-9e658601edc1d17dc97bc281105c01afb13c9855-webp-450x450.jpg'
-            }
-          ]
-        },
-        {
-          status: 'received',
-          customer: {
-            name: 'Eugene V. Lane',
-            photo: 'http://i.imgur.com/yQaVS1f.jpg'
-          },
-          pickupTime: '8:00 PM',
-          items: [
-            {
-              image: 'http://i5.walmartimages.com/dfw/dce07b8c-1b24/k2-_6883b400-953a-4171-9da3-3cac0b502f47.v1.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-ff07/k2-_2a609791-dce8-4b9c-8e18-2ea4b11ba22c.v3.jpg-b9a07f3ea413c5a38f76a34ba7365e1404207a83-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-20a3/k2-_64c47dad-5d13-4f20-82fb-caa3a793d9ab.v1.jpg-5873a66dd7e0ed3ac2db50252fa0ec12f9c30999-webp-450x450.jpg'
-            },
-            {
-              image: 'http://ll-us-i5.wal.co/dfw/dce07b8c-5433/k2-_30a6bd22-4a19-4be8-bdb3-490d3ae31a4c.v6.jpg-9e658601edc1d17dc97bc281105c01afb13c9855-webp-450x450.jpg'
-            }
-          ]
-        }
-      ])
+      dataSource: ds.cloneWithRows(mockData)
     };
+  }
+
+  onUpdateItem(item) {
+    console.log(find(mockData, item));
   }
 
   onPressRow(item) {
@@ -161,7 +176,8 @@ class EmptyPage extends React.Component {
       title: `#${item.walmartNumber}`,
       component: ProductPage,
       passProps: {
-        item: item
+        item: item,
+        onUpdateItem: this.onUpdateItem
       }
     })
 
@@ -266,7 +282,7 @@ class EmptyPage extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{ flex:1 }}>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderRow.bind(this)}
@@ -378,16 +394,15 @@ var styles = StyleSheet.create({
   },
   item: {
     marginRight: 5,
-    marginVertical: 5,
     //borderWidth: 1,
     //borderColor: '#00ff00',
-    borderRadius: 6,
     width: 40,
     height: 40
   },
   itemOverlay: {
     flex: 1,
-    backgroundColor: 'rgba(46,204,113,0.5)'
+    borderRadius: 6,
+    backgroundColor: 'rgba(46,204,113,0.3)'
   },
   innerContainer: {
     borderRadius: 10,
@@ -435,8 +450,9 @@ var styles = StyleSheet.create({
   },
   productImage: {
     flex: 1,
-    width: 450,
-    height: 450,
+    resizeMode: 'contain',
+    width: 220,
+    height: 220,
     marginBottom: 10
   },
   productTitle: {
@@ -484,41 +500,14 @@ var styles = StyleSheet.create({
   }
 });
 
-var Button = React.createClass({
-  getInitialState() {
-    return {
-      active: false,
-    };
-  },
-
-  _onHighlight() {
-    this.setState({ active: true });
-  },
-
-  _onUnhighlight() {
-    this.setState({ active: false });
-  },
-
-  render() {
-    var colorStyle = {
-      color: this.state.active ? '#fff' : '#000'
-    };
-    return (
-      <TouchableHighlight
-        onHideUnderlay={this._onUnhighlight}
-        onPress={this.props.onPress}
-        onShowUnderlay={this._onHighlight}
-        style={[styles.button, this.props.style]}
-        underlayColor="#a9d9d4">
-        <Text style={[styles.buttonText, colorStyle]}>{this.props.children}</Text>
-      </TouchableHighlight>
-    );
-  }
-});
-
 class ProductPage extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  onPressDone(item) {
+    this.props.onUpdateItem(item);
+    this.props.navigator.pop();
   }
 
   render() {
@@ -531,7 +520,7 @@ class ProductPage extends React.Component {
             style={styles.productImage}
             source={{ uri: item.image }}/>
           <Text style={styles.productTitle}>{item.title.toUpperCase()}</Text>
-          <Text style={styles.productNumber}>Walmart #{item.walmartNumber}</Text>
+          <Text style={styles.productNumber}>{item.category}</Text>
           <Text style={styles.productPrice}>{'$' + parseFloat(item.price).toFixed(2)}</Text>
         </View>
 
@@ -543,7 +532,7 @@ class ProductPage extends React.Component {
             <Text style={styles.productQty}>Qty: {item.quantity}</Text>
           </TouchableOpacity>
         </View>
-        <TouchableHighlight style={styles.markAsDone}>
+        <TouchableHighlight style={styles.markAsDone} onPress={this.onPressDone.bind(this, item)}>
           <Text style={styles.markAsDoneText}>MARK AS DONE</Text>
         </TouchableHighlight>
       </View>
